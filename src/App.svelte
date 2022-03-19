a90c022b99fd0c7065fb3f0bc28a2a01d787cbb6
nanangbro <bintangfreak31@gmail.com>
2022-03-18 21:50:15 -0700

    local state

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let name;
+
+	let count = 0;
+	
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<main>
+	<h1>Hi {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={increment}>
+		count: {count}
+	</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
